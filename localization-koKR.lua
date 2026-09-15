5e853b46fda25088130140ff0a136cd92bc0de71
GrazyMonster <monster@diegamer.com>
2026-09-15 09:38:07 +0200

    Update localization-koKR.lua

--- localization-koKR.lua
+++ localization-koKR.lua
@@ -21,4 +21,30 @@ MBB_HELP3 = "  |c00ffffffreset position|r: MBB 미니맵 위치를 초기화 합
 MBB_HELP4 = "  |c00ffffffreset all|r: 모두 기본설정으로 되돌립니다.";
 MBB_NOERRORS = "오류를 찾을수 없습니다!";
 
+
+MBB_FIRST_RUN_TITLE = "MinimapButtonBag Reborn을 불러왔습니다."
+MBB_FIRST_RUN_COUNT = "미니맵 버튼 %d개를 수집했습니다."
+MBB_FIRST_RUN_HELP = "사용 가능한 명령어를 보려면 /mbb를 입력하세요."
+MBB_FIRST_RUN_RESCAN = "버튼이 누락된 경우 /mbb rescan을 사용하세요."
+
+MBB_ABOUT_TITLE = "MinimapButtonBag Reborn";
+MBB_ABOUT_VERSION = "버전:";
+MBB_ABOUT_BASED_ON = "(4.0.28 기반)";
+MBB_ABOUT_CREDITS_TITLE = "제작진:";
+
+MBB_PATCH_STATUS_TITLE = "MinimapButtonBag Reborn – 패치 상태";
+MBB_PATCH_ADDON_INTERFACE = "애드온 인터페이스 버전:";
+MBB_PATCH_WOW_INTERFACE = "현재 WoW 인터페이스 버전:";
+MBB_PATCH_OK = "상태: 이 WoW 패치에 맞는 최신 애드온입니다.";
+MBB_PATCH_OLD = "상태: 이 애드온은 이전 WoW 패치용입니다. 업데이트를 확인하세요.";
+
+MBB_HELP_ABOUT = "  |c00ffffffabout|r: 애드온 정보(버전 및 제작진)를 표시합니다";
+MBB_HELP_PATCH = "  |c00ffffffpatch|r: 패치 호환성 상태를 표시합니다";
+MBB_HELP_RESCAN = "  |c00ffffffrescan|r: 누락된 미니맵 버튼을 다시 검색합니다";
+MBB_RESCAN_DONE = "MBB: 재검색 완료. 새로운 미니맵 버튼 %d개를 찾았습니다.";
+
+MBB_OPTIONS_DETACH = "미니맵에서 분리";
+MBB_OPTIONS_DETACH_DESC = "MBB 버튼을 화면에서 자유롭게 이동할 수 있습니다.";
+MBB_OPTIONS_DETACH_COMBAT = "전투 중에는 변경할 수 없습니다.";
+
 end
