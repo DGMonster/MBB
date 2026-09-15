cc2b210d511d3b5662c994a42fc072d8c07db914
GrazyMonster <monster@diegamer.com>
2026-09-15 09:38:25 +0200

    Update localization-ruRU.lua

--- localization-ruRU.lua
+++ localization-ruRU.lua
@@ -21,4 +21,30 @@ MBB_HELP3 = "  |c00ffffffreset position|r: сбросить позицию MBB �
 MBB_HELP4 = "  |c00ffffffreset all|r: сбросить все опции";
 MBB_NOERRORS = "Ошибок не найдено!";
 
+
+MBB_FIRST_RUN_TITLE = "MinimapButtonBag Reborn загружен."
+MBB_FIRST_RUN_COUNT = "Собрано кнопок миникарты: %d."
+MBB_FIRST_RUN_HELP = "Введите /mbb, чтобы увидеть доступные команды."
+MBB_FIRST_RUN_RESCAN = "Используйте /mbb rescan, если некоторые кнопки отсутствуют."
+
+MBB_ABOUT_TITLE = "MinimapButtonBag Reborn";
+MBB_ABOUT_VERSION = "Версия:";
+MBB_ABOUT_BASED_ON = "(на основе 4.0.28)";
+MBB_ABOUT_CREDITS_TITLE = "Авторы:";
+
+MBB_PATCH_STATUS_TITLE = "MinimapButtonBag Reborn – Статус патча";
+MBB_PATCH_ADDON_INTERFACE = "Версия интерфейса аддона:";
+MBB_PATCH_WOW_INTERFACE = "Текущая версия интерфейса WoW:";
+MBB_PATCH_OK = "Статус: аддон актуален для этого патча WoW.";
+MBB_PATCH_OLD = "Статус: этот аддон создан для более старого патча WoW. Проверьте наличие обновлений.";
+
+MBB_HELP_ABOUT = "  |c00ffffffabout|r: Показать информацию об аддоне (версия и авторы)";
+MBB_HELP_PATCH = "  |c00ffffffpatch|r: Показать статус совместимости с патчем";
+MBB_HELP_RESCAN = "  |c00ffffffrescan|r: Повторно найти отсутствующие кнопки миникарты";
+MBB_RESCAN_DONE = "MBB: повторное сканирование завершено. Найдено новых кнопок миникарты: %d.";
+
+MBB_OPTIONS_DETACH = "Отсоединить от миникарты";
+MBB_OPTIONS_DETACH_DESC = "Позволяет свободно перемещать кнопку MBB по экрану.";
+MBB_OPTIONS_DETACH_COMBAT = "Нельзя изменить во время боя.";
+
 end
