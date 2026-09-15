1b45e11320f6bc0c630df5dcb4dbcce4567d283b
GrazyMonster <monster@diegamer.com>
2026-09-15 09:37:21 +0200

    Update localization-frFR.lua

--- localization-frFR.lua
+++ localization-frFR.lua
@@ -18,5 +18,31 @@ if( GetLocale() == "frFR" ) then
 	MBB_HELP2 = "  |c00ffffffbuttons|r: Affiche la liste de tous les boutons de la barre MBB";
 	MBB_HELP3 = "  |c00ffffffreset position|r: Reinitialiser la position du bouton MBB";
 	MBB_HELP4 = "  |c00ffffffreset all|r: Reinitialiser toutes les options";
-	MBB_NOERRORS = "Pas d'erreurs trouv�es!";
+	MBB_NOERRORS = "Aucune erreur trouvée !";
+
+	MBB_FIRST_RUN_TITLE = "MinimapButtonBag Reborn chargé."
+	MBB_FIRST_RUN_COUNT = "%d boutons de minicarte collectés."
+	MBB_FIRST_RUN_HELP = "Tapez /mbb pour voir les commandes disponibles."
+	MBB_FIRST_RUN_RESCAN = "Utilisez /mbb rescan si des boutons sont manquants."
+
+	MBB_ABOUT_TITLE = "MinimapButtonBag Reborn";
+	MBB_ABOUT_VERSION = "Version :";
+	MBB_ABOUT_BASED_ON = "(basé sur 4.0.28)";
+	MBB_ABOUT_CREDITS_TITLE = "Crédits :";
+
+	MBB_PATCH_STATUS_TITLE = "MinimapButtonBag Reborn – État du patch";
+	MBB_PATCH_ADDON_INTERFACE = "Version d’interface de l’addon :";
+	MBB_PATCH_WOW_INTERFACE = "Version actuelle de l’interface WoW :";
+	MBB_PATCH_OK = "État : l’addon est à jour pour ce patch de WoW.";
+	MBB_PATCH_OLD = "État : cet addon a été conçu pour un ancien patch de WoW. Veuillez vérifier les mises à jour.";
+
+	MBB_HELP_ABOUT = "  |c00ffffffabout|r: Affiche les informations de l’addon (version et crédits)";
+	MBB_HELP_PATCH = "  |c00ffffffpatch|r: Affiche l’état de compatibilité avec le patch";
+	MBB_HELP_RESCAN = "  |c00ffffffrescan|r: Recherche à nouveau les boutons manquants sur la minicarte";
+	MBB_RESCAN_DONE = "MBB : nouvelle analyse terminée. %d nouveau(x) bouton(s) de minicarte trouvé(s).";
+
+	MBB_OPTIONS_DETACH = "Détacher de la minicarte";
+	MBB_OPTIONS_DETACH_DESC = "Permet de déplacer librement le bouton MBB à l’écran.";
+	MBB_OPTIONS_DETACH_COMBAT = "Ne peut pas être modifié pendant le combat.";
+
 end
