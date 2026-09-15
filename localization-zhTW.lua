803303c971e2d41c88d249e474e984a568ce67f0
GrazyMonster <monster@diegamer.com>
2026-09-15 09:39:07 +0200

    Update localization-zhTW.lua

--- localization-zhTW.lua
+++ localization-zhTW.lua
@@ -21,4 +21,30 @@ MBB_HELP3 = "  |c00ffffffreset position|r: 重設 MBB 在小地圖的位置";
 MBB_HELP4 = "  |c00ffffffreset all|r: 重設所有選項";
 MBB_NOERRORS = "沒有發現錯誤!";
 
+
+MBB_FIRST_RUN_TITLE = "MinimapButtonBag Reborn 已載入。"
+MBB_FIRST_RUN_COUNT = "已收集 %d 個小地圖按鈕。"
+MBB_FIRST_RUN_HELP = "輸入 /mbb 查看可用指令。"
+MBB_FIRST_RUN_RESCAN = "如果有按鈕遺漏，請使用 /mbb rescan。"
+
+MBB_ABOUT_TITLE = "MinimapButtonBag Reborn";
+MBB_ABOUT_VERSION = "版本：";
+MBB_ABOUT_BASED_ON = "（基於 4.0.28）";
+MBB_ABOUT_CREDITS_TITLE = "致謝：";
+
+MBB_PATCH_STATUS_TITLE = "MinimapButtonBag Reborn – 更新狀態";
+MBB_PATCH_ADDON_INTERFACE = "插件介面版本：";
+MBB_PATCH_WOW_INTERFACE = "目前 WoW 介面版本：";
+MBB_PATCH_OK = "狀態：插件已支援目前的 WoW 更新版本。";
+MBB_PATCH_OLD = "狀態：此插件是為較舊的 WoW 更新版本建立，請檢查是否有更新。";
+
+MBB_HELP_ABOUT = "  |c00ffffffabout|r: 顯示插件資訊（版本與致謝）";
+MBB_HELP_PATCH = "  |c00ffffffpatch|r: 顯示更新版本相容狀態";
+MBB_HELP_RESCAN = "  |c00ffffffrescan|r: 重新掃描小地圖以尋找遺漏的按鈕";
+MBB_RESCAN_DONE = "MBB：重新掃描完成。找到 %d 個新的小地圖按鈕。";
+
+MBB_OPTIONS_DETACH = "從小地圖分離";
+MBB_OPTIONS_DETACH_DESC = "允許在畫面上自由移動 MBB 按鈕。";
+MBB_OPTIONS_DETACH_COMBAT = "戰鬥中無法變更。";
+
 end
